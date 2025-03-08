2205309aa66b63f644719294af0a7fd95830ef38
Nanda Arya Putra <nandaarya404@gmail.com>
2025-03-08 07:58:28 +0700

    Clean the code. clean second screen in template

--- src/main/kotlin/com/github/nandaarya/cleanarchitecturetemplate/ProjectRecipe.kt
+++ src/main/kotlin/com/github/nandaarya/cleanarchitecturetemplate/ProjectRecipe.kt
@@ -1,37 +1,35 @@
 package com.github.nandaarya.cleanarchitecturetemplate
 
-import com.android.tools.idea.wizard.template.Language
 import com.android.tools.idea.wizard.template.ModuleTemplateData
 import com.android.tools.idea.wizard.template.PackageName
 import com.android.tools.idea.wizard.template.RecipeExecutor
 import com.android.tools.idea.wizard.template.impl.activities.common.generateSimpleLayout
-import com.android.tools.idea.wizard.template.impl.activities.emptyActivity.src.emptyActivityJava
-import com.github.nandaarya.cleanarchitecturetemplate.source.*
 import com.github.nandaarya.cleanarchitecturetemplate.source.configuration.androidManifestXml
 import com.github.nandaarya.cleanarchitecturetemplate.source.configuration.buildGradleKt
 import com.github.nandaarya.cleanarchitecturetemplate.source.configuration.libsVersionsToml
-import com.github.nandaarya.cleanarchitecturetemplate.source.local.*
+import com.github.nandaarya.cleanarchitecturetemplate.source.emptyActivityKt
+import com.github.nandaarya.cleanarchitecturetemplate.source.local.daoKt
+import com.github.nandaarya.cleanarchitecturetemplate.source.local.databaseKt
+import com.github.nandaarya.cleanarchitecturetemplate.source.local.entityKt
+import com.github.nandaarya.cleanarchitecturetemplate.source.myApplicationKt
 import com.github.nandaarya.cleanarchitecturetemplate.source.remote.apiConfigKt
 import com.github.nandaarya.cleanarchitecturetemplate.source.remote.apiServiceKt
 import com.github.nandaarya.cleanarchitecturetemplate.source.remote.registerResponseKt
 
 fun RecipeExecutor.projectRecipe(
     moduleData: ModuleTemplateData,
-    activityClass: String,
-    generateLayout: Boolean,
-    layoutName: String,
     packageName: PackageName,
 ) {
     val (projectData, srcOut) = moduleData
     val useAndroidX = projectData.androidXSupport
-    val ktOrJavaExt = projectData.language.extension
 
     applyPlugin("com.google.devtools.ksp", "2.1.0-1.0.29")
     applyPlugin("com.google.dagger.hilt.android", "2.55")
 
-    if (generateLayout) {
-        generateSimpleLayout(moduleData, "ui.$activityClass", layoutName, containerId = "main")
-    }
+    val activityClass = "MainActivity"
+    val layoutName = "activity_main"
+
+    generateSimpleLayout(moduleData, "ui.$activityClass", layoutName, containerId = "main")
 
     createDirectory(moduleData.srcDir.resolve("ui"))
     createDirectory(moduleData.srcDir.resolve("data"))
@@ -90,11 +88,9 @@ fun RecipeExecutor.projectRecipe(
     val registerResponse = registerResponseKt(packageName)
     save(registerResponse, registerResponsePath)
 
-    val simpleActivity = when (projectData.language) {
-        Language.Kotlin -> emptyActivityKt(packageName, moduleData.namespace, activityClass, layoutName, generateLayout, useAndroidX)
-        Language.Java -> emptyActivityJava(packageName, moduleData.namespace, activityClass, layoutName, generateLayout, useAndroidX)
-    }
-    val simpleActivityPath = srcOut.resolve("ui/$activityClass.$ktOrJavaExt")
+    val simpleActivityPath = srcOut.resolve("ui/MainActivity.kt")
+    val simpleActivity =
+        emptyActivityKt(packageName, moduleData.namespace, activityClass, layoutName, true, useAndroidX)
     save(simpleActivity, simpleActivityPath)
     open(simpleActivityPath)
 }
--- src/main/kotlin/com/github/nandaarya/cleanarchitecturetemplate/ProjectTemplate.kt
+++ src/main/kotlin/com/github/nandaarya/cleanarchitecturetemplate/ProjectTemplate.kt
@@ -17,38 +17,38 @@ val projectTemplate
         screens = listOf(WizardUiContext.NewProject, WizardUiContext.NewProjectExtraDetail)
         constraints = listOf(TemplateConstraint.AndroidX, TemplateConstraint.Material3, TemplateConstraint.Kotlin)
 
-        val generateLayout: BooleanParameter = booleanParameter {
-            name = "Generate a Layout File"
-            default = true
-            help = "If true, a layout file will be generated"
-        }
-        lateinit var layoutName: StringParameter
-        val activityClass: StringParameter = stringParameter {
-            name = "Activity Name"
-            constraints = listOf(CLASS, UNIQUE, NONEMPTY)
-            suggest = {
-                layoutToActivity(layoutName.value)
-            }
-            default = "MainActivity"
-            help = "The name of the activity class to create"
-        }
-        layoutName = stringParameter {
-            name = "Layout Name"
-            constraints = listOf(LAYOUT, UNIQUE, NONEMPTY)
-            suggest = {
-                activityToLayout(activityClass.value)
-            }
-            default = "activity_main"
-            visible = { generateLayout.value }
-            help = "The name of the UI layout to create for the activity"
-        }
+//        val generateLayout: BooleanParameter = booleanParameter {
+//            name = "Generate a Layout File"
+//            default = true
+//            help = "If true, a layout file will be generated"
+//        }
+//        lateinit var layoutName: StringParameter
+//        val activityClass: StringParameter = stringParameter {
+//            name = "Activity Name"
+//            constraints = listOf(CLASS, UNIQUE, NONEMPTY)
+//            suggest = {
+//                layoutToActivity(layoutName.value)
+//            }
+//            default = "MainActivity"
+//            help = "The name of the activity class to create"
+//        }
+//        layoutName = stringParameter {
+//            name = "Layout Name"
+//            constraints = listOf(LAYOUT, UNIQUE, NONEMPTY)
+//            suggest = {
+//                activityToLayout(activityClass.value)
+//            }
+//            default = "activity_main"
+//            visible = { generateLayout.value }
+//            help = "The name of the UI layout to create for the activity"
+//        }
         val packageName = defaultPackageNameParameter
 
         widgets(
             PackageNameWidget(packageName),
-            TextFieldWidget(activityClass),
-            CheckBoxWidget(generateLayout),
-            TextFieldWidget(layoutName),
+//            TextFieldWidget(activityClass),
+//            CheckBoxWidget(generateLayout),
+//            TextFieldWidget(layoutName),
         )
 
         thumb {
@@ -58,9 +58,9 @@ val projectTemplate
         recipe = { data ->
             projectRecipe(
                 data as ModuleTemplateData,
-                activityClass.value,
-                generateLayout.value,
-                layoutName.value,
+//                activityClass.value,
+//                generateLayout.value,
+//                layoutName.value,
                 packageName.value,
             )
         }
