503035194b69aa42440efc93891770a1e2013d11
Nanda Arya Putra <nandaarya404@gmail.com>
2025-04-24 18:22:58 +0700

    change local to create platform

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,8 +45,8 @@ dependencies {
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        local("C:\\Program Files\\Android\\Android Studio")
-        // create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
+        // local("C:\\Program Files\\Android\\Android Studio")
+        create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
 
         // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
         bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
